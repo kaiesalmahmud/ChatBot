02a5be0465f6b838981fdead31b9e2e29d152d32
Kaies Al Mahmud <kaies.rmedu03@gmail.com>
2023-03-01 17:10:31 +0600

    updated

--- actions/actions.py
+++ actions/actions.py
@@ -27,85 +27,85 @@
 #         return []
 
 
-from typing import Any, Text, Dict, List
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-import requests
+# from typing import Any, Text, Dict, List
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+# import requests
 
-class ActionRequest(Action):
+# class ActionRequest(Action):
 
-    def name(self) -> Text:
-        return "action_request"
+#     def name(self) -> Text:
+#         return "action_request"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        # extract necessary entities from user input
-        leave_type = tracker.get_slot("leave_type")
-        start_date = tracker.get_slot("start_date")
-        end_date = tracker.get_slot("end_date")
+#         # extract necessary entities from user input
+#         leave_type = tracker.get_slot("leave_type")
+#         start_date = tracker.get_slot("start_date")
+#         end_date = tracker.get_slot("end_date")
 
-        # send request to HRIS system
-        # replace this with your own code to interact with your HRIS system
-        response = requests.post(url="http://hris-system.com/leave_request",
-                                 data={
-                                       "leave_type": leave_type,
-                                       "start_date": start_date,
-                                       "end_date": end_date
-                                    })
+#         # send request to HRIS system
+#         # replace this with your own code to interact with your HRIS system
+#         response = requests.post(url="http://hris-system.com/leave_request",
+#                                  data={
+#                                        "leave_type": leave_type,
+#                                        "start_date": start_date,
+#                                        "end_date": end_date
+#                                     })
         
-        # send response message to user
-        message = "Your {} leave request from {} to {} has been submitted.".format(leave_type, start_date, end_date)
-        dispatcher.utter_message(text=message)
+#         # send response message to user
+#         message = "Your {} leave request from {} to {} has been submitted.".format(leave_type, start_date, end_date)
+#         dispatcher.utter_message(text=message)
 
-        return []
+#         return []
 
-class ActionClaim(Action):
+# class ActionClaim(Action):
 
-    def name(self) -> Text:
-        return "action_claim"
+#     def name(self) -> Text:
+#         return "action_claim"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        # extract necessary entities from user input
-        claim_type = tracker.get_slot("claim_type")
-        amount = tracker.get_slot("amount")
+#         # extract necessary entities from user input
+#         claim_type = tracker.get_slot("claim_type")
+#         amount = tracker.get_slot("amount")
 
-        # send request to HRIS system
-        # replace this with your own code to interact with your HRIS system
-        response = requests.post(url="http://hris-system.com/claim_request",
-                                 data={
-                                       "claim_type": claim_type,
-                                       "amount": amount
-                                    })
+#         # send request to HRIS system
+#         # replace this with your own code to interact with your HRIS system
+#         response = requests.post(url="http://hris-system.com/claim_request",
+#                                  data={
+#                                        "claim_type": claim_type,
+#                                        "amount": amount
+#                                     })
 
-        # send response message to user
-        message = "Your {} claim request of {} has been submitted.".format(claim_type, amount)
-        dispatcher.utter_message(text=message)
+#         # send response message to user
+#         message = "Your {} claim request of {} has been submitted.".format(claim_type, amount)
+#         dispatcher.utter_message(text=message)
 
-        return []
+#         return []
 
-class ActionAnalytics(Action):
+# class ActionAnalytics(Action):
 
-    def name(self) -> Text:
-        return "action_analytics"
+#     def name(self) -> Text:
+#         return "action_analytics"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        # get the necessary data from HRIS system
-        # replace this with your own code to interact with your HRIS system
-        data = requests.get(url="http://hris-system.com/analytics")
+#         # get the necessary data from HRIS system
+#         # replace this with your own code to interact with your HRIS system
+#         data = requests.get(url="http://hris-system.com/analytics")
 
-        # generate analytics report
-        # replace this with your own code to generate the analytics report
-        report = "Here is your analytics report: {}".format(data)
+#         # generate analytics report
+#         # replace this with your own code to generate the analytics report
+#         report = "Here is your analytics report: {}".format(data)
 
-        # send response message to user
-        dispatcher.utter_message(text=report)
+#         # send response message to user
+#         dispatcher.utter_message(text=report)
 
-        return []
+#         return []
